31213b4ea5db18ad7d04e30fe1857604ff1e6ea3
vivekbondla <vivekbondla95@gmail.com>
2023-08-02 20:53:32 +0530

    Changed to promise for getting all books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -17,9 +18,13 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async (req, res) => {
   //Write your code here
-  return res.status(200).send(JSON.stringify({books},null,4));
+  const result = new Promise((resolve,reject)=>{
+    resolve(books)
+  })
+  const value = await result
+  return res.status(200).send(JSON.stringify({books:value},null,4));
 });
 
 // Get book details based on ISBN
